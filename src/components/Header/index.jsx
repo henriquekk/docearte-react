2b749034f78596ea1662f58210fddc31f9c9fdaf
henriquekk <henrique.guerrato@gmail.com>
2023-06-26 13:14:19 -0300

    add Login Modal

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,13 @@ import "./Header.css"
 import logo from 'assets/images/logo.png'
 import { AiOutlineUser } from 'react-icons/ai';
 import { BsCart3 } from 'react-icons/bs';
+import Modal from "components/Modal";
+import { useState } from "react";
 
 export default function Header(){
+
+  const [openModal, setOpenModal] = useState(false)
+
   return(
     <header>
       <div className="header-area">
@@ -15,7 +20,8 @@ export default function Header(){
         <nav>
           <ul>
             <li><BsCart3 size="30px" color="#fff"/></li>
-            <li><AiOutlineUser size="30px" color="#fff"/></li>
+            <li><button className="login-btn" onClick={() => setOpenModal(true)}><AiOutlineUser size="30px" color="#fff"/></button></li>
+            <Modal isOpen={openModal} closeModal={() => setOpenModal(!openModal)}/>
           </ul>
         </nav>
       </div>
