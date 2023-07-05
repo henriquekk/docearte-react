774c22c108f173064cb91aada9083d95ee7ea870
henriquekk <henrique.guerrato@gmail.com>
2023-07-05 18:21:19 -0300

    adjust Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import SearchBar from "components/SearchBar";
 import "./Header.css"
 import logo from 'assets/images/logo.png'
-import { AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineHome } from 'react-icons/ai';
 import { BsCart3 } from 'react-icons/bs';
 import Modal from "components/Modal";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header(){
 
@@ -19,7 +20,8 @@ export default function Header(){
         
         <nav>
           <ul>
-            <li><BsCart3 size="30px" color="#fff"/></li>
+            <li><Link to="/"><AiOutlineHome size="30px" color="#fff"/></Link></li>
+            <li><Link to="/carrinho"><BsCart3 size="30px" color="#fff"/></Link></li>
             <li><button className="login-btn" onClick={() => setOpenModal(true)}><AiOutlineUser size="30px" color="#fff"/></button></li>
             <Modal isOpen={openModal} closeModal={() => setOpenModal(!openModal)}/>
           </ul>
