1ccc1133f3982fec0a6aafa1dc757f4b4296275c
henriquekk <henrique.guerrato@gmail.com>
2023-07-12 18:32:39 -0300

    adicionando paginas

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import HomePage from "pages/HomePage";
 import Carrinho from "pages/Carrinho";
 import Produtos from "pages/Produtos";
 import Compra from 'pages/Compra';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 export default function App() {
-  
   return (
-
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/produto/:id" element={<Compra />}/>
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/carrinho" element={<Carrinho />} />
         <Route path="/produto" element={<Compra />} />
+        <Route path="*" element={<NaoEncontrada/>}></Route>
       </Routes>
     </Router>
   );
