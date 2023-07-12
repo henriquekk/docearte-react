dd224d2fc8b1819f8c700204c5a2ca8bfc4d5215
henriquekk <henrique.guerrato@gmail.com>
2023-07-12 18:35:08 -0300

    add Pagina do produto

--- src/components/Produto/index.jsx
+++ src/components/Produto/index.jsx
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './Produto.css';
 import ContentContainer from 'components/ContentContainer';
-import imagemDoProduto from 'assets/images/produtos/prod-1.png'
 import Button from 'components/Button';
+import client from 'util/datocms';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 export default function Produto() {
+  const { id } = useParams();
+  const [produto, setProduto] = useState(null);
+
+  useEffect(() => {
+    const fetchProduto = async () => {
+      try {
+        const query = `
+          {
+            produto(filter: { id: { eq: "${id}" } }) {
+              id
+              titulo
+              preco
+              categoria
+              imagem {
+                url
+              }
+              descricao
+            }
+          }
+        `;
+
+        const { produto } = await client.request(query);
+        setProduto(produto);
+      } catch (error) {
+        console.error('Erro ao obter o produto:', error);
+      }
+    };
+
+    fetchProduto();
+  }, [id]);
+
+  if (!produto) {
+    return <NaoEncontrada />;
+  }
+
+ 
+
   return (
     <>
       <ContentContainer>
         <div className='product-area'>
           <div className='product-box'>
-            <img src={imagemDoProduto} alt=""/>
+            <img src={produto.imagem.url} alt={produto.titulo} />
             <div className='product-info'>
-              <h1 className='titulo-compra'>Nome do produto</h1>
-              <p className='preco-compra'>R$ 40,00</p>
+              <h1 className='titulo-compra'>{produto.titulo}</h1>
+              <p className='preco-compra'>{produto.preco}</p>
             </div>
-           
           </div>
           <div className='descricao-compra'>
             <h1>Descrição do produto</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius eos, totam voluptates doloribus sit corporis accusantium nostrum repudiandae cumque nihil ad corrupti, dolorum suscipit obcaecati libero consequuntur! A, impedit ex!</p>
+            <p>{produto.descricao}</p>
             <Button>
               Adicionar ao carrinho
             </Button>
           </div>
         </div>
-        
-
-        
-        
-
       </ContentContainer>
     </>
   )
