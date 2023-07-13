df7986190b47bee68cb3990f8e6935a5ba9cce54
henriquekk <henrique.guerrato@gmail.com>
2023-07-13 00:27:02 -0300

    adjust SearchBox

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,13 +1,14 @@
 import './SearchBar.css';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { Link } from 'react-router-dom'
 
 export default function SearchBar(){
   return(
     <div className="search-bar">
       <input type="text" className="search-text" placeholder="Pesquisar"/>
-      <a href="" className="search-button">
+      <Link to="/produtos/" className="search-button">
         <AiOutlineSearch size="40px" color='#19C8DB'/>
-      </a>
+      </Link>
     </div> 
   );
 }
